a2b037a4271f4bfd0021ce6b9f23eead42aecfca
Jack Bowden <jackbowden1992@gmail.com>
2024-07-30 19:46:09 +0100

    Visual changes - Image

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,20 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-	plugins:
-		process.env.NODE_ENV === 'production'
-			? [
-					'autoprefixer',
-					[
-						'@fullhuman/postcss-purgecss',
-						{
-							content: ['./{pages,components}/**/*.{js,jsx}'],
-							safelist: ['html', 'body'],
-							defaultExtractor: content =>
-								content.match(/[\w-/:]+(?<!:)/g) || [],
-						},
-					],
-			  ]
-			: ['autoprefixer'],
+	plugins: {
+		tailwindcss: {},
+	},
 };
 
 export default config;
